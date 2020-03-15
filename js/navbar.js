e2010d34dd0643b00853b9c0c15625d7bac8d995
Mykola <mr.gigami@gmail.com>
2020-03-15 12:25:57 +0000

    nav script

---
+++ js/navbar.js
@@ -0,0 +1,15 @@
+let nav = '<ul class="nav">' +
+              '<li class="nav-item">'+
+                 '<a class="nav-link" href="index.html">Home</a>'+
+              '</li>'+
+              '<li class="nav-item">'+
+                 '<a class="nav-link" href="html/quiz.html">Quiz</a>'+
+              '</li>'+
+              '<li class="nav-item">'+
+                 '<a class="nav-link" href="html/kontakt.html">Kontakt</a>'+
+              '</li>'+
+          '</ul>';
+
+let navbarElem = document.querySelector(".navbar");
+
+navbarElem.innerHTML = nav;
